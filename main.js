19510b9b2d8665185122cd4fbb88786ed8409286
fogelmark <108455196+fogelmark@users.noreply.github.com>
2022-10-15 18:29:09 +0200

    fixade en switch

--- main.js
+++ main.js
@@ -1,131 +1,142 @@
-const form = document.querySelector('#validationForm')
-const firstName = document.querySelector('#firstName')
-const lastName = document.querySelector('#lastName')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-const repeatPassword = document.querySelector('#repeatPassword')
-const errorMsg = document.querySelector('#errorMessage')
+const form = document.querySelector("#validationForm");
+const errorMsg = document.querySelector("#errorMessage");
 
+const validateFirstName = (id) => {
+  const name = document.querySelector(id);
+  const regEx = /^[a-z]+$/i
 
-const validateName = (id) => {
-    const input = document.querySelector(id)
-    
-    if (input.value.trim() === '' ||
-        input.value.length < 2 ||
-        input.value.match(0-9)) {
-        console.error('Invalid name');
-        return false
-    } else {
-        console.log(input.value);
-        return true
-    }
-}
+  if (
+    name.value.trim() === "" ||
+    name.value.length < 2 ||
+    !name.value.match(regEx)
+  ) {
+    console.error("Invalid first name");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const validateLastName = (id) => {
+  const name = document.querySelector(id);
+  const regEx = /^[a-z]+$/i
+
+  if (
+    name.value.trim() === "" ||
+    name.value.length < 2 ||
+    !name.value.match(regEx)
+  ) {
+    console.error("Invalid last name");
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const validateEmail = (id) => {
-    const email = document.querySelector(id)
-    
-    const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/
+  const email = document.querySelector(id);
 
-    if(!regEx.test(email.value)) {
-       console.error('Invalid email');
-       return false
-    } else {
-         console.log(email.value);
-         return true
-    }
-}
+  const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+
+  if (!regEx.test(email.value)) {
+    console.error("Invalid email");
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const validatePassword = (id) => {
-    const password = document.querySelector(id)
-    
-    const regEx = /^[A-Za-z]\w{5,11}$/ // password måste vara minst 6, max 12 tecken
+  const password = document.querySelector(id);
+  const regEx = /^[A-Za-z]\w{5,11}$/; // password måste vara minst 6, max 12 tecken
 
-    if (!password.value.match(regEx)) {
-        console.error('Password must contain 6-12 signs');
-    } else {
-        console.log(password.value);
-    }
-}
+  if (!password.value.match(regEx)) {
+    console.error("Password must contain 6-12 signs");
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const validateRepeatPassword = (id) => {
-    const repeatPassword = document.querySelector(id)
+  const password = document.querySelector("#password");
+  const repeatPassword = document.querySelector(id);
 
-    if (repeatPassword.value != password.value) {
-        console.error('Passwords does not match');
-    } else {
-        return true
-    }
-}
-
+  if (repeatPassword.value != password.value) {
+    console.error("Passwords does not match");
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const validateTerms = (id) => {
-    const terms = document.querySelector(id)
+  const terms = document.querySelector(id);
 
-    if (!terms.checked) {
-        console.error('Terms not accepted');
-        return false
-    } else {
-        console.log('Terms accepted');
-        return true
-    }
-}
-
-const setSuccess = () => {
-    errorMsg.classList.add('d-none')
-    console.log('Validation complete');
-    return true;
-}
-const setError = () => {
-    errorMsg.classList.remove('d-none')
-    console.error('Alla fält måste vara korrekt ifyllda!');
+  if (!terms.checked) {
+    console.error("Terms not accepted");
     return false;
-}
+  } else {
+    return true;
+  }
+};
 
+const valSuccess = () => {
+  errorMsg.classList.add("d-none");
+  console.log("Validation complete");
+  return true;
+};
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
+const valError = () => {
+  errorMsg.classList.remove("d-none");
+  console.error("Alla fält måste vara korrekt ifyllda!");
+  return false;
+};
 
-    const errors = []
-    console.clear();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    validateName('#firstName')
-    validateName('#lastName')
-    validateEmail('#email')
-    validatePassword('#password')
-    validateRepeatPassword('#repeatPassword')
-    validateTerms('#terms')
+  console.clear(); // Clearar console så det blir lättare vid error testing
+  const errors = [];
 
-    
-    // for(let i = 0; i < form.length; i++) {
+  for (let i = 0; i < form.length; i++) {
 
-    //     const validateId = '#' + form[i].id
+    const validateId = "#" + form[i].id;
 
-    //     if (form[i].type === 'text') {
-    //         errors[i] = validateName(validateId)
-    //     } else if (form[i].type === 'email') {
-    //         errors[i] = validateEmail(validateId)
-    //     } else if (form[i].type === 'password') {
-    //         errors[i] = validatePassword(validateId)
-    //     } else if (form[i].type === 'password') {
-    //         errors[i] = validateRepeatPassword(validateId)
-    //     } else if (form[i].type === 'checkbox') {
-    //         errors[i] = validateTerms(validateId)
-    //     }
+    switch (validateId) {
+      case "#firstName":
+        errors[i] = validateFirstName(validateId);
+        break;
+      case "#lastName":
+        errors[i] = validateLastName(validateId);
+        break;
+      case "#email":
+        errors[i] = validateEmail(validateId);
+        break;
+      case "#password":
+        errors[i] = validatePassword(validateId);
+        break;
+      case "#repeatPassword":
+        errors[i] = validateRepeatPassword(validateId);
+        break;
+      case "#terms":
+        errors[i] = validateTerms(validateId);
+        break;
+      default:
+        break;
+    }
+  }
 
-    // }
-
-    //     if (errors.includes(false)) {
-    //         setError()
-    //     } else {
-    //         const user = {
-    //             firstName: firstName.value,
-    //             lastName: lastName.value,
-    //             email: email.value,
-    //             password: password.value
-    //         }
-    //         setSuccess()
-    //         console.log(user);
-    //     }
-    //     console.log(errors);
-
-    })
+  if (errors.includes(false)) {
+    valError();
+  } else {
+    const user = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
+    };
+    valSuccess();
+    console.log(user);
+  }
+});
